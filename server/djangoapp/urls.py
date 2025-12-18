58e0ad9206060ef3318b88f673065af71ca6ff43
94Vish <vellalagoda94@gmail.com>
2025-12-18 02:49:04 -0500

    dealer changes 1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,5 +18,16 @@ urlpatterns = [
     # path for add a review view
 
     path(route='get_cars', view=views.get_cars, name ='getcars'),
-
+    
+    # Path for dealerships
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    
+    # Path for dealer details
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    
+    # Path for dealer reviews
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),
+    # Path for add reviews
+    path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
